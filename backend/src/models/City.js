7bbefa2c77a79ce779eab8d03bccd6fe0d074593
Mr Rahul <ashish227087@gmail.com>
2026-03-10 23:12:03 +0530

    feat: add City model

---
+++ backend/src/models/City.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+  const citySchema = new mongoose.Schema =({
+
+    name:{
+      type :String,
+      require:true
+    },
+    sate:{
+      type:String,
+      require:true
+    },
+    description:{
+      type:String
+    },
+    image:{
+    type:String
+    }
+  },
+  {
+    timestamps:true
+  }
+)
+ const City = mongoose.model("City",citySchema)
+
+ export default City
