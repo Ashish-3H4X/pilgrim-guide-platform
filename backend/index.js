f3f9e7ba2a363423b2630b2427816404de680683
Mr Rahul <ashish227087@gmail.com>
2026-03-10 23:00:14 +0530

    feat1 : setup express server with basic route feat2 : add mongodb connection configuration

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
 
 dotenv.config();
 
@@ -20,3 +21,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+connectDB();
