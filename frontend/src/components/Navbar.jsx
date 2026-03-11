18079caa6581fa1d84fb3f9ca1086eb2266af782
Mr Rahul <ashish227087@gmail.com>
2026-03-12 00:21:42 +0530

    feat: add navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+function Navbar() {
+
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+
+      <div className="max-w-6xl mx-auto flex justify-between">
+
+        <h1 className="text-xl font-bold">
+          Pilgrim Guide
+        </h1>
+
+        <div>
+          Cities
+        </div>
+
+      </div>
+
+    </nav>
+  );
+
+}
+
+export default Navbar;
