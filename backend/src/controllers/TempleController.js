58c3b549ba80217996cb4ee57f522d729506a127
Mr Rahul <ashish227087@gmail.com>
2026-03-11 22:57:46 +0530

    refactor: update temple controller with filtering support

--- backend/src/controllers/TempleController.js
+++ backend/src/controllers/TempleController.js
@@ -1,12 +1,32 @@
 import Temple from "../models/Temple.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
-export const getTemples = async (req, res) => {
-  const temples = await Temple.find().populate("city");
-  res.json(temples);
-};
+export const getTemples = asyncHandler(async (req, res) => {
 
-export const createTemple = async (req, res) => {
+  const filter = {};
+
+  if (req.query.city) {
+    filter.city = req.query.city;
+  }
+
+  const temples = await Temple.find(filter).populate("city");
+
+  res.json({
+    success: true,
+    count: temples.length,
+    data: temples
+  });
+
+});
+
+export const createTemple = asyncHandler(async (req, res) => {
+
   const temple = new Temple(req.body);
   await temple.save();
-  res.json(temple);
-};
+
+  res.status(201).json({
+    success: true,
+    data: temple
+  });
+
+});
