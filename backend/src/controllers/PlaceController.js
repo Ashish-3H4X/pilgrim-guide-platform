7594aa17664ea67e264ff6e0e9098b5840d52468
Mr Rahul <ashish227087@gmail.com>
2026-03-11 22:58:33 +0530

    refactor: update place controller with async handler

--- backend/src/controllers/PlaceController.js
+++ backend/src/controllers/PlaceController.js
@@ -1,12 +1,32 @@
 import Place from "../models/Place.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
-export const getPlaces = async (req, res) => {
-  const places = await Place.find().populate("city");
-  res.json(places);
-};
+export const getPlaces = asyncHandler(async (req, res) => {
 
-export const createPlace = async (req, res) => {
+  const filter = {};
+
+  if (req.query.city) {
+    filter.city = req.query.city;
+  }
+
+  const places = await Place.find(filter).populate("city");
+
+  res.json({
+    success: true,
+    count: places.length,
+    data: places
+  });
+
+});
+
+export const createPlace = asyncHandler(async (req, res) => {
+
   const place = new Place(req.body);
   await place.save();
-  res.json(place);
-};
+
+  res.status(201).json({
+    success: true,
+    data: place
+  });
+
+});
