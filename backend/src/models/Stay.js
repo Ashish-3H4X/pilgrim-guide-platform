bc01f39dc222f31aa1bc80be4be7c301f86c312b
Mr Rahul <ashish227087@gmail.com>
2026-03-11 21:06:13 +0530

    feat: add stay model for dharamshala and hotels

---
+++ backend/src/models/Stay.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const StaySchema = new mongoose.Schema(
+  {
+    name: String,
+
+    city: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "City"
+    },
+
+    type: {
+      type: String
+    },
+
+    price: Number,
+
+    location: {
+      lat: Number,
+      lng: Number
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Stay = mongoose.model("Stay", StaySchema);
+
+export default Stay;
