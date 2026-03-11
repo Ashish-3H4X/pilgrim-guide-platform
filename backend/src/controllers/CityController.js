bff7a57ae8dafad9e6ef64676d626676b403cbf3
Mr Rahul <ashish227087@gmail.com>
2026-03-11 22:56:43 +0530

    refactor: update city controller with async handler

--- backend/src/controllers/CityController.js
+++ backend/src/controllers/CityController.js
@@ -1,12 +1,26 @@
 import City from "../models/City.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
-export const getCities = async (req, res) => {
+export const getCities = asyncHandler(async (req, res) => {
+
   const cities = await City.find();
-  res.json(cities);
-};
 
-export const createCity = async (req, res) => {
+  res.json({
+    success: true,
+    count: cities.length,
+    data: cities
+  });
+
+});
+
+export const createCity = asyncHandler(async (req, res) => {
+
   const city = new City(req.body);
   await city.save();
-  res.json(city);
-};
+
+  res.status(201).json({
+    success: true,
+    data: city
+  });
+
+});
