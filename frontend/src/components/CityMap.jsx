d2553e2b0dcd208df1dda058dc4078396ebeec29
Mr Rahul <ashish227087@gmail.com>
2026-03-12 22:12:24 +0530

    feat: implement interactive city map with temple markers

--- frontend/src/components/CityMap.jsx
+++ frontend/src/components/CityMap.jsx
@@ -1,10 +1,47 @@
-function CityMap() {
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+function CityMap({ temples }) {
+
+  const defaultPosition = [27.5035, 77.6737];
 
   return (
 
-    <div className="bg-gray-200 h-80 rounded-lg flex items-center justify-center">
+    <div className="h-96 rounded-lg overflow-hidden mb-8">
 
-      Map will appear here
+      <MapContainer
+        center={defaultPosition}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+      >
+
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {temples.map((temple) => (
+
+          temple.location && (
+
+            <Marker
+              key={temple._id}
+              position={[
+                temple.location.lat,
+                temple.location.lng
+              ]}
+            >
+
+              <Popup>
+                {temple.name}
+              </Popup>
+
+            </Marker>
+
+          )
+
+        ))}
+
+      </MapContainer>
 
     </div>
 
@@ -13,3 +50,4 @@ function CityMap() {
 }
 
 export default CityMap;
+
