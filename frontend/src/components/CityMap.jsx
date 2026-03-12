0bdf056663452eb3c05d61c1de9696b8245e0991
Mr Rahul <ashish227087@gmail.com>
2026-03-12 22:01:57 +0530

    feat: add city map placeholder component

---
+++ frontend/src/components/CityMap.jsx
@@ -0,0 +1,15 @@
+function CityMap() {
+
+  return (
+
+    <div className="bg-gray-200 h-80 rounded-lg flex items-center justify-center">
+
+      Map will appear here
+
+    </div>
+
+  );
+
+}
+
+export default CityMap;
